7b6ebb42f32c155fbbfe46da123f0a74d1dd43f9
Wendell Liu <cuk.bas@gmail.com>
2016-11-28 12:21:21 +0800

    export for lib form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-
-// import SelectableGroup from './SelectableGroup';
-// import createSeletable from './createSeletable';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 //
-// export default { SelectableGroup, createSeletable}
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+import SelectableGroup from './SelectableGroup';
+import createSeletable from './createSeletable';
+
+export default { SelectableGroup, createSeletable }
