aae8a52fbd21b77fcd6a7e2d83bb70108d175e4e
Wendell Liu <cuk.bas@gmail.com>
2018-02-15 15:57:45 +0800

    config

--- config/rollup.demo.config.js
+++ config/rollup.demo.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
+import pkg from '../package.json';
 
 export default [
   // CommonJS (for Node) and ES module (for bundlers) build.
@@ -8,8 +8,8 @@ export default [
   // builds from a single configuration where possible, using
   // the `targets` option which can specify `dest` and `format`)
   {
-    input: 'src/index.js',
-    external: ['react, react-dom', 'prop-types'],
+    input: 'demo/index.js',
+    // external: ['react, react-dom', 'prop-types'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
---
+++ script/buildGhpage.js
@@ -0,0 +1,2 @@
+// const ghpages = require('gh-pages');
+
