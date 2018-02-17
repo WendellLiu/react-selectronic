cb4657db4c3b5d2fbe5d1033882ae2a34fb108df
Wendell Liu <cuk.bas@gmail.com>
2018-02-17 12:50:01 +0800

    index.html to demo dir

--- script/buildDemo.js
+++ script/buildDemo.js
@@ -1,10 +1,14 @@
 const rollup = require('rollup');
+const fse = require('fs-extra');
+
 const { inputOptions, outputOptions } = require('./demoConfig');
 
-async function build() {
-  const bundle = await rollup.rollup(inputOptions);
+async function main() {
+  await fse.emptyDir('static');
+  await fse.copy('public', 'static');
 
+  const bundle = await rollup.rollup(inputOptions);
   await bundle.write(outputOptions);
 }
 
-build();
+main();
--- script/develop.js
+++ script/develop.js
@@ -1,4 +1,6 @@
 const rollup = require('rollup');
+const serve = require('serve');
+const fse = require('fs-extra');
 const { inputOptions, outputOptions } = require('./demoConfig');
 
 
@@ -6,40 +8,46 @@ const watchOptions = {
   ...inputOptions,
   output: outputOptions,
 };
-const watcher = rollup.watch(watchOptions);
 
-// event.code
-//   START        — the watcher is (re)starting
-//   BUNDLE_START — building an individual bundle
-//   BUNDLE_END   — finished building a bundle
-//   END          — finished building all bundles
-//   ERROR        — encountered an error while bundling
-//   FATAL        — encountered an unrecoverable error
-watcher.on('event', async (event) => {
-  switch (event.code) {
-    case 'START':
-      console.log('START');
-      break;
-    case 'BUNDLE_START':
-      console.log('BUNDLE_START');
-      break;
-    case 'BUNDLE_END':
-      console.log('BUNDLE_END');
-      break;
-    case 'END':
-      console.log('END');
-      break;
-    case 'ERROR':
-      console.log('ERROR');
-      break;
-    case 'FATAL':
-      console.log('FATAL');
-      break;
-    default:
-      console.log('something else');
-      break;
-  }
-});
+async function main() {
+  await fse.emptyDir('static');
+  await fse.copy('public', 'static');
+  const watcher = await rollup.watch(watchOptions);
+  await serve('static');
 
-// stop watching
-// watcher.close();
+  // event.code
+  //   START        — the watcher is (re)starting
+  //   BUNDLE_START — building an individual bundle
+  //   BUNDLE_END   — finished building a bundle
+  //   END          — finished building all bundles
+  //   ERROR        — encountered an error while bundling
+  //   FATAL        — encountered an unrecoverable error
+  watcher.on('event', async (event) => {
+    switch (event.code) {
+      case 'START':
+        console.log('START');
+        break;
+      case 'BUNDLE_START':
+        console.log('BUNDLE_START');
+        break;
+      case 'BUNDLE_END':
+        console.log('BUNDLE_END');
+        break;
+      case 'END':
+        console.log('END');
+        break;
+      case 'ERROR':
+        console.log('ERROR');
+        break;
+      case 'FATAL':
+        console.log('FATAL');
+        break;
+      default:
+        console.log('something else');
+        break;
+    }
+  });
+}
+
+
+main();
